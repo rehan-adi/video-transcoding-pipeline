a7fdf1796a66c0feb4ff07ccc6cfa8d6c6ca2bc9
Rehan <rehanalire52@gmail.com>
2025-01-21 19:56:44 +0530

    feat: Implement file upload functionality with multer

--- upload-server/src/controllers/upload.ts
+++ upload-server/src/controllers/upload.ts
@@ -1,7 +1,13 @@
+import s3 from "../utils/s3";
 import { Request, Response } from "express";
 
 export const uploadFile = async (req: Request, res: Response) => {
   try {
+    console.log(req.file);
+
+    res
+      .status(200)
+      .json({ success: true, message: "Video uploaded successfully" });
   } catch (error) {
     console.error(error, "Error uploading file");
     res.status(500).json({ success: false, message: "Internal Server Error" });
--- upload-server/src/routes/upload.ts
+++ upload-server/src/routes/upload.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import { upload } from "../utils/multer";
 import { uploadFile } from "../controllers/upload";
 
 export const uploadRouter = express.Router();
 
-uploadRouter.post("/", uploadFile);
+uploadRouter.post("/", upload.single("video"), uploadFile);

2c717d0c40659dc774283d94514a14913186b3f2
Rehan <rehanalire52@gmail.com>
2025-01-22 13:33:39 +0530

    feat: Update S3 integration to use AWS SDK v3 and improve file upload handling

--- upload-server/src/utils/s3.ts
+++ upload-server/src/utils/s3.ts
@@ -1,9 +1,12 @@
-import AWS from "aws-sdk";
+import env from "dotenv";
+import { S3Client } from "@aws-sdk/client-s3";
 
-const s3 = new AWS.S3({
-    region: "us-east-1",
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+env.config();
+
+export const s3 = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
 });
-
-export default s3;
